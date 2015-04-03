dd6dababb20c9368f5cc68d35d070859f69b106a
rajmatiearjune <stephanie@graphicsflo.com>
2015-04-02 21:26:09 -0400

    user input
    adding fields for user to input information when prompted

--- madlib/main.py
+++ madlib/main.py
@@ -4,9 +4,20 @@
 
 print "Hello!! Welcome to my Madlib Game!"
 
+#my story
+number = raw_input("Enter your number between 30 to 85?")
+animal = raw_input("What is your favorite animal?")
+noun = raw_input("Enter a noun?")
+verb = raw_input("Enter a verb?")
+adjective = raw_input("Enter an adjective.")
+door = raw_input("What is the opposite of open?")
 
 
 
+print "Once there was an old women who was " + number + " years old. " + " One day she found a " + animal + " and accidentally " + verb
+
+
+"""
 def myGame():
 	playerSelect = dict()
 	addSelect('city', playerSelect)
@@ -14,11 +25,11 @@ def myGame():
 	addSelect('hero', playerSelect)
 	game = gameFormat.format(**playerSelect)
 	
-	
-def playerSelect(cue, dictionary):
-	'''Prompt for player response using the cue string and place the cue response pair in the dictionary '''
-	prompt = 'Enter a cue ' + cue + ': '
-	response = input(prompt)
-	dictionary [cue] = response
-	myGame()
-	print "Once upon a time " + myGame
+
+
+
+print "Roses are " + adjective1
+print noun1 + " are blue"
+print noun2 + " is " + adjective2
+print "And so are you!"
+"""
